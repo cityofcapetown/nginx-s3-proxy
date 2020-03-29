e6496cf82aa29309a24fc8b29b513b0d6c0878f1
Gordon Inggs <gordon.inggs@gmail.com>
2020-03-29 15:03:35 +0200

    Bug fix for run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-set -e
 
 echo "$(date -Iminutes) Injecting Env Variables."
 /inject-env-vars.sh $ACCESS_KEY $SECRET_KEY $BUCKET_NAME $PROXY_PASS_HOST $PROXY_HEADER_HOST
 
 echo "$(date -Iminutes) Starting Nginx Proxy."
-/usr/local/nginx/sbin/nginx -g 'daemon off;'
+/usr/local/nginx/sbin/nginx -c /nginx.conf
