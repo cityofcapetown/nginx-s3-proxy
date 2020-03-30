0e22b8ef0da7d1b61ff497c657542ab1601757ac
Gordon Inggs <gordon.inggs@gmail.com>
2020-03-30 23:03:56 +0200

    Revert "Revert "Adding root html redirect option.""
    This reverts commit 51478593

--- bin/inject-env-vars.sh
+++ bin/inject-env-vars.sh
@@ -2,10 +2,11 @@
 
 ACCESS_KEY="$1"
 SECRET_KEY="$2"
-BUCKET_NAME=${3:-}
+BUCKET_NAME="$3"
 
-PROXY_PASS_HOST=${4:-https://172.29.100.54}
-PROXY_HEADER_HOST=${5:-ds2.capetown.gov.za}
+ROOT_HTML_PATH=${4:-index.html}
+PROXY_PASS_HOST=${5:-https://172.29.100.54}
+PROXY_HEADER_HOST=${6:-ds2.capetown.gov.za}
 
 echo "Replac[ing] template variables..."
 
@@ -13,6 +14,7 @@ cp /nginx.conf /tmp/nginx.conf.template
 sed -i "s/<ACCESS_KEY GOES HERE>/"$ACCESS_KEY"/" /tmp/nginx.conf.template
 sed -i "s/<SECRET_KEY GOES HERE>/"$SECRET_KEY"/" /tmp/nginx.conf.template
 sed -i "s/<BUCKET_NAME GOES HERE>/"$BUCKET_NAME"/" /tmp/nginx.conf.template
+sed -i "s/<ROOT_HTML_PATH GOES HERE>/"$ROOT_HTML_PATH"/" /tmp/nginx.conf.template
 sed -i "s#<PROXY_PASS_HOST GOES HERE>#"$PROXY_PASS_HOST"#" /tmp/nginx.conf.template
 sed -i "s/<PROXY_HEADER_HOST GOES HERE>/"$PROXY_HEADER_HOST"/" /tmp/nginx.conf.template
 cp /tmp/nginx.conf.template /nginx.conf
--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "$(date -Iminutes) Injecting Env Variables."
-/inject-env-vars.sh $ACCESS_KEY $SECRET_KEY $BUCKET_NAME $PROXY_PASS_HOST $PROXY_HEADER_HOST
+/inject-env-vars.sh $ACCESS_KEY $SECRET_KEY $BUCKET_NAME $ROOT_HTML_PATH $PROXY_PASS_HOST $PROXY_HEADER_HOST
 
 echo "$(date -Iminutes) Starting Nginx Proxy."
 /usr/local/nginx/sbin/nginx -c /nginx.conf
