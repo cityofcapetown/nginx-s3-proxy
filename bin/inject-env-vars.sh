89d945bc9ab956d91dd88dfaa0dc82be3ce14dae
Gordon Inggs <gordon.inggs@gmail.com>
2020-03-30 13:59:06 +0200

    Doing templating on a separate file
    Avoids sed device busy error.

--- bin/inject-env-vars.sh
+++ bin/inject-env-vars.sh
@@ -9,10 +9,12 @@ PROXY_HEADER_HOST=${5:-ds2.capetown.gov.za}
 
 echo "Replac[ing] template variables..."
 
-sed -i "s/<ACCESS_KEY GOES HERE>/"$ACCESS_KEY"/" /nginx.conf
-sed -i "s/<SECRET_KEY GOES HERE>/"$SECRET_KEY"/" /nginx.conf
-sed -i "s/<BUCKET_NAME GOES HERE>/"$BUCKET_NAME"/" /nginx.conf
-sed -i "s#<PROXY_PASS_HOST GOES HERE>#"$PROXY_PASS_HOST"#" /nginx.conf
-sed -i "s/<PROXY_HEADER_HOST GOES HERE>/"$PROXY_HEADER_HOST"/" /nginx.conf
+cp /nginx.conf /tmp/nginx.conf.template
+sed -i "s/<ACCESS_KEY GOES HERE>/"$ACCESS_KEY"/" /tmp/nginx.conf.template
+sed -i "s/<SECRET_KEY GOES HERE>/"$SECRET_KEY"/" /tmp/nginx.conf.template
+sed -i "s/<BUCKET_NAME GOES HERE>/"$BUCKET_NAME"/" /tmp/nginx.conf.template
+sed -i "s#<PROXY_PASS_HOST GOES HERE>#"$PROXY_PASS_HOST"#" /tmp/nginx.conf.template
+sed -i "s/<PROXY_HEADER_HOST GOES HERE>/"$PROXY_HEADER_HOST"/" /tmp/nginx.conf.template
+cp /tmp/nginx.conf.template /nginx.conf
 
 echo "... Replac[ed] template variables."
